c7829c1d8555dc47765fd5c817774859a7a0e950
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-01-05 18:50:37 +0530

    05-01-2025

---
+++ Task6/script.js
@@ -0,0 +1,19 @@
+var divs = document.querySelectorAll('.tab');
+var texts = document.querySelectorAll('h3');
+
+texts[0].style.display = "block";
+texts[0].style.width = "50%";
+
+divs.forEach(function (div, index) {
+    div.addEventListener("click", function () {
+        hideAllText();
+        texts[index].style.display = "block";
+        texts[index].style.width = "50%";
+    });
+});
+
+function hideAllText() {
+    texts.forEach(function (text) {
+        text.style.display = "none";
+    });
+}
