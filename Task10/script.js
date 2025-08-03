3662f57109d12d837cc5501dded3b89ca48fd597
Amruta Dahatonde <amrutadahatonde10@gmail.com>
2025-08-03 11:18:11 +0530

    Update script.js

--- Task10/script.js
+++ Task10/script.js
@@ -1,6 +1,6 @@
 const progressBar = document.getElementById('progress-bar');
 
-    window.onscroll = function () {
+    window.onscroll = function () { 
       const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
       const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
       const progress = (scrollTop / scrollHeight) * 100;
